de5f3ebf4bf8d47f3e007beea30b68585659dc92
StratosMylonas <stratosmylonas@yahoo.gr>
2025-12-29 18:06:40 +0200

    title fix

--- public/jar/script.js
+++ public/jar/script.js
@@ -1,6 +1,7 @@
 const socket = io({ path: "/socket.io", timeout: 2000 });
 
 let connected = false;
+let fixedTitle = "Donation Goal"; // will be set once
 
 socket.on("connect", () => {
     connected = true;
@@ -12,22 +13,28 @@ setTimeout(() => {
     }
 }, 2500);
 
+/* Initial load */
 socket.on("donation-goal", (data) => {
-    updateBar(data.currentDonations, data.goal, data.title);
+    fixedTitle = data.title || fixedTitle;
+    setTitle(fixedTitle);
+    updateBar(data.currentDonations, data.goal);
 });
 
+/* Donation updates */
 socket.on("donation-update", (data) => {
-    updateBar(data.totalDonations, data.goal, data.title);
+    updateBar(data.totalDonations, data.goal);
 });
 
-function updateBar(amount, goal, title = "Donation Goal") {
+function setTitle(title) {
+    document.getElementById("bar-title").innerText = title;
+}
+
+function updateBar(amount, goal) {
     const bar = document.getElementById("donation-bar");
     const barAmount = document.getElementById("bar-amount");
-    const barTitle = document.getElementById("bar-title");
 
     const percentage = Math.min(amount / goal, 1) * 100;
 
     bar.style.width = `${percentage}%`;
-    barTitle.innerText = title;
     barAmount.innerText = `${amount.toFixed(2)} / ${Math.floor(goal)}€ (${percentage.toFixed(0)}%)`;
 }
